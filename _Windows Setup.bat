ccd5c61aa635cf18cbf6e1098ef8dce71b9c3368
YSG <YSG@YSGS-DESKTOP-20>
2023-04-30 05:46:18 -0400

    Fixed python versioning issues.

--- _Windows Setup.bat
+++ _Windows Setup.bat
@@ -4,12 +4,9 @@ TITLE ISAAC - Instant Service Account Adder ^& Configurator - Windows Edition
 echo WARNING: do NOT run this on your headless server ^!
 echo Make sure you have your oauth json saved to directory before proceeding (it doesnt have to be named credentials.json, it will be auto renamed if it starts with "client")
 echo. 
-pause
+python-3.11.3.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 Include_pip=1
 del backup /s /q >> SetupLog.txt
-winget install -e --id Python.Python.3.10
-set PATH=%PATH%;%USERPROFILE%\AppData\Local\Programs\Python\Python310\Scripts;%USERPROFILE%\AppData\Local\Programs\Python\Python310
 mkdir backup >> SetupLog.txt
-
 cls
 echo Backing up directory, removing previous backup, and cleaning any files leftover from previous runs and cleaning up directory...
 timeout /t 6 /nobreak > nul
@@ -21,7 +18,7 @@ del /s /q ".\credentials\*.pickle"  >> SetupLog.txt
 del /s /q ".\*.pickle"  >> SetupLog.txt
 rem Install Properties-updates, I believe this was a key piece I missed last time.
 move .\client*.json .\credentials.json >> SetupLog.txt
-if "%ERRORLEVEL%" == "1" (goto :CheckForCredentials) else (goto :Continue)
+if "%ERRORLEVEL%" == "1" (goto :CheckForCredentials) else (goto :C ontinue)
 
 :CheckForCredentials
 if NOT EXIST ".\credentials.json" (goto :ExitNow) else (goto :Continue)
@@ -38,26 +35,23 @@ echo.
 echo.
 timeout /t 5
 exit 1
-
+]
 :Continue
 @ECHO off
-"%USERPROFILE%\AppData\Local\Programs\Python\Python310\Scripts\pip3.exe" install google-api-python-client google-auth-httplib2 google-auth-oauthlib
-"%USERPROFILE%\AppData\Local\Programs\Python\Python310\Scripts\pip3.exe" install -r "%~dp0requirements.txt" 
-"%USERPROFILE%\AppData\Local\Programs\Python\Python310\python.exe" -m pip install --upgrade pip
-cls
+pip install google-api-python-client google-auth-httplib2 google-auth-oauthlib
+pip install -r "%~dp0requirements.txt" 
+python -m pip install --upgrade pip
 echo Next you must confirm authorization of the SA account generation script, copy and paste the link into your browser, or if you're using terminal just ctrl+click it.
 echo.
 echo.
-"%USERPROFILE%\AppData\Local\Programs\Python\Python310\python.exe" gen_sa_accounts.py 
+python gen_sa_accounts.py 
 echo. 
 echo.
-
 :START
 set /a COUNT=0
 setlocal EnableDelayedExpansion
-cls
 echo Choose which account to add the SA's to:
-FOR /F "tokens=* USEBACKQ" %%F IN (`"%USERPROFILE%\AppData\Local\Programs\Python\Python310\python.exe" gen_sa_accounts.py --list-projects`) DO (
+FOR /F "tokens=* USEBACKQ" %%F IN (`python gen_sa_accounts.py --list-projects`) DO (
 set /a COUNT=COUNT+1
 set var=%%F
 if "!COUNT!"=="2" (echo 1 - %%F & set "PROJID=%%F" & set CHOICE1=%%F)
@@ -94,8 +88,6 @@ echo Incorrect answer given... & timeout /t 5 & goto :isokay
 
 
 :ProjectIDChosen
-:DeleteSas
-cls
 echo Would you like to delete previous Service Accounts on this account (i.e. you have lost the files)[Y/N]?
 set /p delsas=
 if "%delsas%" EQU "Y" goto :delsas
@@ -105,14 +97,18 @@ if "%delsas%" EQU "N" goto :ADDSAS
 echo Incorrect answer given... & timeout /t 5 & cls & goto :DeleteSas
 
 :delsas
-"%USERPROFILE%\AppData\Local\Programs\Python\Python310\python.exe" gen_sa_accounts.py --delete-sas "%PROJID%"
+python.exe gen_sa_accounts.py --delete-sas "%PROJID%"
 
 :ADDSAS
-"%USERPROFILE%\AppData\Local\Programs\Python\Python310\python.exe" gen_sa_accounts.py --enable-services "%PROJID%"
-"%USERPROFILE%\AppData\Local\Programs\Python\Python310\python.exe" gen_sa_accounts.py --create-sas "%PROJID%"
-"%USERPROFILE%\AppData\Local\Programs\Python\Python310\python.exe" gen_sa_accounts.py --download-keys "%PROJID%"
-"%USERPROFILE%\AppData\Local\Programs\Python\Python310\python.exe" rename_script.py
-"%USERPROFILE%\AppData\Local\Programs\Python\Python310\python.exe" GetEmails.py
+python gen_sa_accounts.py --enable-services "%PROJID%"
+python gen_sa_accounts.py --create-sas "%PROJID%"
+python gen_sa_accounts.py --download-keys "%PROJID%"
+echo Enter your group email address now, this is given when you create the group in your admin panel.
+set /p groupmail=
+python add_to_google_group.py -g %groupmail%
+
+python rename_script.py
+python GetEmails.py
 copy ".\accounts\0.json" ".\accounts\100.json" >> SetupLog.txt
 copy ".\token.pickle" ".\credentials\token.pickle" >> SetupLog.txt
 
--- rename_script.py
+++
@@ -1,8 +0,0 @@
-import os
-base = "accounts"
-count = 0
-for file in os.listdir(base):
-    abspath = os.path.join(base,file)
-    destpath = os.path.join(base,f"{count}.json")
-    os.rename(abspath,destpath)
-    count += 1
